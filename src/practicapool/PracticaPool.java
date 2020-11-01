e913a13804ab74ac456ba46b992193f49760bc51
Miguel Angel Vazquez Pech <36494384+MiguelAngelVazquezPech@users.noreply.github.com>
2020-11-01 15:36:31 -0600

    Add files via upload

---
+++ src/practicapool/PracticaPool.java
@@ -0,0 +1,11 @@
+
+package practicapool;
+
+public class PracticaPool {
+
+    public static void main(String[] args) {
+        frmPrincipal objetoprincipal = new frmPrincipal();
+        objetoprincipal.setVisible(true);
+    }
+    
+}
